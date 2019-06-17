773aa0b8508a85443392c6088b82f7820671662e
RangiBecerra <rangibecerra@gmail.com>
2019-06-16 21:31:13 -0400

    nuevo componente para mapa

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
+import Map from './map';
 
 function App() {
   const main = new Date().toTimeString();
   return (
     <div className="App">
       {main}
+      <Map/>
     </div>
   );
 }
---
+++ my-app/src/map.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class Map extends Component {
+  render() {
+    return (
+      <p>Mapa</p>
+    );
+  };
+};
+
+export default Map;
